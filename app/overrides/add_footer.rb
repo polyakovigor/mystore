7b7c073b2857e2a82d41e40714c99a927199b882
Igor <igor.polyakov@faceit-team.com>
2017-10-19 18:44:03 +0300

    fix footer

--- app/overrides/add_footer.rb
+++ app/overrides/add_footer.rb
@@ -1,4 +1,6 @@
 Deface::Override.new(virtual_path: 'spree/layouts/spree_application',
+                     name: 'add_footer',
                      insert_after: 'div.container',
                      partial: 'spree/shared/footer',
-                     name: 'add_footer')
+                     disabled: false,
+                     original: 'c5300405404a01ada5323e7942bdfc6a11cae3a4')
